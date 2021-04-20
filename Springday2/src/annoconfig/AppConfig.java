c443edfd46ef31b24f41c6120920ba0c927bae40
k-selvam <selvam11699@gmail.com>
2021-04-20 12:46:25 +0530

    annotation beans

---
+++ Springday2/src/annoconfig/AppConfig.java
@@ -0,0 +1,17 @@
+package annoconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public Samsung getPhone() {
+		return new Samsung();
+	}
+	@Bean
+	public MobileProcessor getCpu() {
+		return new Snapdragon();
+	}
+}
