5c12add4b60261d879e56f8864854bbd5f67dccf
k-selvam <selvam11699@gmail.com>
2021-04-21 14:15:28 +0530

    all methods which take string as an argument

--- Springday2/src/aop/main/LoggingAspect.java
+++ Springday2/src/aop/main/LoggingAspect.java
@@ -5,6 +5,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
+import aop.model.Circle;
+
 @Aspect
 public class LoggingAspect {
 	//one aspect can contain multiple advices
@@ -19,31 +21,40 @@ public class LoggingAspect {
 	//@Before("execution(public String aop.model.*.getName())")  --- for all getName methods in all classes within the package aop.model
 	//@Before("execution(public * aop.model.*.getName())")  --- irrespective of return type
 	//@Before("execution(public * aop.model.*.get*())")   --- for all methods which start with get
-	
-	
-	//@Before("execution(* aop..*.get*())")  == 	@Before("allGetters()")
-	
-	@Before("allCircleMethods()")
-	public void loggingAdvice(JoinPoint joinPoint) {
-		String methodName = joinPoint.toLongString();
-		if(methodName.contains("getDia")) {
-		System.out.println("writing log for getdia method b4 its executed");
-		}
-		else 
-			if(methodName.contains("setName")) {
-				System.out.println("writing log for setName method b4 its executed");
 
-				
-			}
-	}
+
+	//@Before("execution(* aop..*.get*())")  == 	@Before("allGetters()")
+
+	/*
+	 * @Before("allCircleMethods()") public void loggingAdvice(JoinPoint joinPoint)
+	 * { String methodName = joinPoint.toLongString();
+	 * 
+	 * if(methodName.contains("getDia")) { Circle circle = (Circle)
+	 * joinPoint.getTarget(); System.out.println(circle.getName());
+	 * System.out.println("writing log for getdia method b4 its executed");
+	 * 
+	 * } else if(methodName.contains("setName")) {
+	 * System.out.println("writing log for setName method b4 its executed");
+	 * 
+	 * 
+	 * } }
+	 */
 
 	@Pointcut("execution(* aop..*.get*())")
 	public void allGetters() {}
-	
+
 	@Pointcut("within(aop.model.Circle)")
 	public void allCircleMethods() {}
 	
-	
+	//@Before("args(name)")
+	@Before("methodsStringArgs(name)")
+	public void stringArgsAdvice(String name) {
+		System.out.println("advice for all methods which accept a string as an argument "+name);
+	}
+
+	@Pointcut("args(name)")
+	public void methodsStringArgs(String name) {}
+
 
 	/*
 	 * @Before("execution(* aop..*.get*())") public void secondAdviceforAllGetters()
@@ -54,7 +65,7 @@ public class LoggingAspect {
 	 * @Before("allGetters") public void thirdAdviceforAllGetters() {
 	 * System.out.println("third log before method is executed"); }
 	 */
-	
+
 
 
 }
